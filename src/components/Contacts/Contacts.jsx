658959d9ca849423e2a8892ca84f72d23403207c
Constantine <buzuev.k@mail.ru>
2020-09-14 00:46:34 +0400

    fix(contacts): fix layout

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -21,36 +21,40 @@ class Contacts extends React.Component {
           styles.contacts,
           this.props.resolution === screen.DESKTOP && styles.contacts_desktop,
           this.props.resolution === screen.LAPTOP && styles.contacts_laptop,
-          this.props.resolution === screen.TABLET && styles.contacts_tablet
+          this.props.resolution === screen.TABLET && styles.contacts_tablet,
+          this.props.resolution === screen.IPHONE && styles.contacts_iphone,
+          this.props.resolution === screen.MOBILE && styles.contacts_mobile
         )}
       >
         <span
           className={classNames(
             styles.contacts__phone,
-            (this.props.resolution === screen.DESKTOP ||
-              this.props.resolution === screen.LAPTOP ||
-              this.props.resolution === screen.TABLET) &&
-              styles.contacts__phone_left,
-            (this.props.resolution === screen.IPHONE ||
-              this.props.resolution === screen.MOBILE) &&
-              styles.contacts__phone_center
+            this.props.resolution === screen.DESKTOP &&
+              styles.contacts__phone_desktop,
+            this.props.resolution === screen.LAPTOP &&
+              styles.contacts__phone_laptop,
+            this.props.resolution === screen.TABLET &&
+              styles.contacts__phone_tablet,
+            this.props.resolution === screen.IPHONE &&
+              styles.contacts__phone_iphone,
+            this.props.resolution === screen.MOBILE &&
+              styles.contacts__phone_mobile
           )}
           children={this.props.phone}
         ></span>
         <span
           className={classNames(
             styles.contacts__address,
-            (this.props.resolution === screen.DESKTOP ||
-              this.props.resolution === screen.LAPTOP ||
-              this.props.resolution === screen.TABLET ||
-              this.props.resolution === screen.IPHONE) &&
-              styles.contacts__address_left,
+            this.props.resolution === screen.DESKTOP &&
+              styles.contacts__address_desktop,
+            this.props.resolution === screen.LAPTOP &&
+              styles.contacts__address_laptop,
+            this.props.resolution === screen.TABLET &&
+              styles.contacts__address_tablet,
+            this.props.resolution === screen.IPHONE &&
+              styles.contacts__address_iphone,
             this.props.resolution === screen.MOBILE &&
-              styles.contacts__address_center,
-            (this.props.resolution === screen.TABLET ||
-              this.props.resolution === screen.IPHONE ||
-              this.props.resolution === screen.MOBILE) &&
-              styles.contacts__address_small
+              styles.contacts__address_mobile
           )}
           children={this.props.address}
         ></span>
