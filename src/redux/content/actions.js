ab816c4be8d5776ca94d2b5e74b7c609a593c1f9
Constantine <buzuev.k@mail.ru>
2020-09-13 16:14:47 +0400

    feat(redux): add redux

--- src/redux/content/actions.js
+++ src/redux/content/actions.js
@@ -1,34 +1,26 @@
-import {
-  SET_VIEW_DESKTOP,
-  SET_VIEW_LAPTOP,
-  SET_VIEW_TABLET,
-  SET_VIEW_IPHONE,
-  SET_VIEW_MOBILE,
-} from '../types'
+import { SET_VIEW, TOGGLE_MENU } from '../types'
 
 import { screen } from '../../data/resolution'
 
-export const setViewDesktop = () => ({
-  type: SET_VIEW_DESKTOP,
-  payload: screen.DESKTOP,
-})
+export const setView = (width) => {
+  let resolution =
+    width >= screen.DESKTOP
+      ? screen.DESKTOP
+      : width >= screen.LAPTOP
+      ? screen.LAPTOP
+      : width >= screen.TABLET
+      ? screen.TABLET
+      : width >= screen.IPHONE
+      ? screen.IPHONE
+      : screen.MOBILE
+  return {
+    type: SET_VIEW,
+    payload: resolution,
+  }
+}
 
-export const setViewLaptop = () => ({
-  type: SET_VIEW_LAPTOP,
-  payload: screen.LAPTOP,
-})
-
-export const setViewTablet = () => ({
-  type: SET_VIEW_TABLET,
-  payload: screen.TABLET,
-})
-
-export const setViewIphone = () => ({
-  type: SET_VIEW_IPHONE,
-  payload: screen.IPHONE,
-})
-
-export const setViewMobile = () => ({
-  type: SET_VIEW_MOBILE,
-  payload: screen.MOBILE,
-})
+export const toggleMenu = () => {
+  return {
+    type: TOGGLE_MENU,
+  }
+}
--- src/redux/content/reducer.js
+++ src/redux/content/reducer.js
@@ -1,29 +1,87 @@
-import {
-  SET_VIEW_DESKTOP,
-  SET_VIEW_LAPTOP,
-  SET_VIEW_TABLET,
-  SET_VIEW_IPHONE,
-  SET_VIEW_MOBILE,
-} from '../types'
+import { SET_VIEW, TOGGLE_MENU } from '../types'
 
 import { screen } from '../../data/resolution'
+import { menu } from '../../data/menu'
 
 const initialState = {
+  phone: '8 (800) 550-88-20',
+  address:
+    'г. Самара, Улица Георгия Димитрова, автостоянка №26 (ориентир Московское шоссе, 205)',
   resolution: screen.DESKTOP,
+  menuShow: false,
+  menuItems: [
+    {
+      name: 'Каталог авто',
+      type: menu.SECTION,
+    },
+    {
+      name: 'Об Автоброкере',
+      type: menu.SECTION,
+    },
+    {
+      name: 'Контакты',
+      type: menu.SECTION,
+    },
+    {
+      name: 'Автокредит',
+      type: menu.SECTION,
+    },
+    {
+      name: 'Услуги',
+      type: menu.SECTION,
+    },
+    {
+      name: 'Срочная продажа',
+      type: menu.SUBSECTION,
+    },
+    {
+      name: 'Комиссионная продажа',
+      type: menu.SUBSECTION,
+    },
+    {
+      name: 'Предпродажная подготовка',
+      type: menu.SUBSECTION,
+    },
+    {
+      name: 'Оценить авто',
+      type: menu.SUBSECTION,
+    },
+    {
+      name: 'Обмен(trade-in)',
+      type: menu.SUBSECTION,
+    },
+    {
+      name: 'ОСАГО',
+      type: menu.SUBSECTION,
+    },
+    {
+      name: 'Продажа с гарантией и обеспечительным платежом',
+      type: menu.SUBSECTION,
+    },
+    {
+      name: 'Онлайн комиссия',
+      type: menu.SUBSECTION,
+    },
+    {
+      name: 'КАСКО',
+      type: menu.SUBSECTION,
+    },
+  ],
 }
 
 export default function resolutionReducer(state = initialState, action) {
   switch (action.type) {
-    case SET_VIEW_DESKTOP:
-    case SET_VIEW_LAPTOP:
-    case SET_VIEW_TABLET:
-    case SET_VIEW_IPHONE:
-    case SET_VIEW_MOBILE: {
+    case SET_VIEW: {
       return {
         ...state,
         resolution: action.payload,
       }
     }
+    case TOGGLE_MENU:
+      return {
+        ...state,
+        menuShow: !state.menuShow,
+      }
     default:
       return state
   }
